8e720bdb41d8bd4a2c409e1f1c0e4ce32ffee6e5
flyover <flyover.gu@gmail.com>
2015-11-07 17:30:30 +0800

    修改登陆方式 & 修改计算得分方法

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 公司内部小项目－用于计算每个人演讲得分和排名
 
 ## 实现接口  
-* /api/login        登入                    post    {user: xxx}
-* /api/saveScore    保存得分                post    {to:xxx, language: 1, ..} 
+* /login            登入                    post    {user: xxx}
+* /api/saveScore    保存得分                post    {to:xxx, xx: 1, ..} 
 * /api/rank         获取全部人员的得分      get                                     {xx: xxx}
 * /api/random       随机所有用户的order     get                                     {xx: xxx}
 * /api/order        获取当前用户的order值   get                                     {order: xxx}
--- app.js
+++ app.js
@@ -6,19 +6,17 @@ let session = require('koa-session');
 let conf = require('./conf.json');
 
 let app = koa();
+let api = '/api/';
+let {port, users, skills}  = conf;
+
 app.keys = ['speech'];
 app.use(mongo({
     host: "localhost",
     port: 27017
 }));
-
 app.use(session(app));
 app.use(bodyparser());
 
-
-let port = 8082;
-let api = '/api/';
-let users = conf.users;
 
 app.use(function*(next) {
     try {
@@ -28,17 +26,22 @@ app.use(function*(next) {
     }
 });
 
-app.use(route.post(api + 'login', function*() {
+app.use(route.post(api + '*', checkLogin));
+app.use(route.get(api + '*', checkLogin));
+
+app.use(route.post('/login', function*() {
     let {
         user
     } = this.request.body;
     if (~users.indexOf(user)) {
         this.session.user = user;
+        this.set('token', user);
         this.body = {
             code: 1
         };
     } else {
         this.session.user = '';
+        this.set('token', '');
         this.body = {
             code: 0,
             msg: 'not find user'
@@ -46,7 +49,6 @@ app.use(route.post(api + 'login', function*() {
     }
 }));
 
-app.use(route.post(api + 'saveScore', checkLogin));
 app.use(route.post(api + 'saveScore', function*() {
     let body = this.request.body;
     let res = {
@@ -73,7 +75,6 @@ app.use(route.post(api + 'saveScore', function*() {
     this.body = res;
 }));
 
-app.use(route.get(api + 'rank', checkLogin));
 app.use(route.get(api + 'rank', function*() {
     let data =
         yield (cb) => {
@@ -83,32 +84,37 @@ app.use(route.get(api + 'rank', function*() {
     this.body = handleResult(h);
 }));
 
-app.use(route.get(api + 'random', checkLogin));
 app.use(route.get(api + 'random', function*() {
     let d = new Date();
-    let time  = d.getFullYear() + '-' + d.getMonth() + '-' + d.getDate();
+    let time = d.getFullYear() + '-' + d.getMonth() + '-' + d.getDate();
     let id = d.getTime();
     let order = randomArray(users);
-    let data = { time: time, id: id, order: order};
+    let data = {
+        time: time,
+        id: id,
+        order: order
+    };
     mo(this.mongo).insert(data);
     this.body = order;
 }));
 
-app.use(route.get(api + 'order', checkLogin));
 app.use(route.get(api + 'order', function*() {
-    let data = yield (cb) => {
-        mo(this.mongo).find().sort({id: -1}).limit(1).toArray((err, res) => cb(err, res));
-    }
+    let data =
+        yield (cb) => mo(this.mongo).find().sort({
+            id: -1
+        }).limit(1).toArray((err, res) => cb(err, res))
     if (data && data[0] && data[0].order) {
         let order = data[0].order;
-        this.body = {order: order.indexOf(this.session.user)}
+        this.body = {
+            order: order.indexOf(this.session.user)
+        };
     } else {
-        this.body = {order: -1};
+        this.body = {
+            order: -1
+        };
     }
 }));
 
-
-app.use(route.get(api + 'toWho', checkLogin));
 app.use(route.get(api + 'toWho', function*() {
     this.body = {
         to: "flyover"
@@ -117,23 +123,18 @@ app.use(route.get(api + 'toWho', function*() {
 
 let handleResult = (data) => {
     let res = {};
+    users.map((user, i) => res[user] = {order: i + 1, score: 0});
     for (let to in data) {
-        res[to] = calculate(data[to]);
-    }
-    for (let i in users) {
-        let user = users[i];
-        if (!res[user]) {
-            res[user] = 0;
-        }
+        res[to] = res[to] || {};
+        Object.assign(res[to], {score: calculate(data[to])});
     }
     return res;
 }
 
-let skills = conf.skills;
 let calculate = (list) => {
     let res = 0;
     list.map((item) => skills.map((p) => res += item[p] ? item[p] : 0));
-    return res;
+    return res / list.length;
 }
 
 let handleData = (data) => {
@@ -147,9 +148,15 @@ let handleData = (data) => {
     return hash;
 }
 
-function* checkLogin(next) {
-    if (this.session.user) {
-        yield next;
+function* checkLogin(path, next) {
+    let user = this.session.user || this.header.token;
+    console.log(user);
+    if (user) {
+        if (typeof path === 'string') {
+            yield next;
+        } else {
+            yield path;
+        }
     } else {
         this.body = {
             code: 0,
@@ -162,9 +169,9 @@ let randomArray = (lists) => {
     if (!lists) return [];
     lists = JSON.parse(JSON.stringify(lists));
     let res = [];
-    while(lists.length) {
+    while (lists.length) {
         let rn = randomInt(lists.length);
-        res.push(lists[rn]); 
+        res.push(lists[rn]);
         lists.splice(rn, 1);
     }
     return res;
